c83a5b4c6f00f8072daf401ef188d080170ea218
Michael Kargl <michael.kargl@reishauer.com>
2021-04-27 11:40:45 +0200

    ✅ Adds unit test for math_helper library

---
+++ test/test_match_helper.c
@@ -0,0 +1,40 @@
+#include "math_helper.h"
+#include "stdio.h"
+#include <unity.h>
+
+void test_function_min(void) {
+    printf("Given two different integers, the smaller one is returned");
+    
+    // Given two numbers
+    int small_number = 12;
+    int big_number = 23;
+
+    // When executing
+    int result = min(small_number, big_number);
+
+    // Then the lower one is returned
+    TEST_ASSERT_EQUAL(small_number, result);
+}
+
+void test_function_max(void) {
+    printf("Given two integers, the bigger one is returned");
+    
+    // Given two numbers
+    int small_number = 12;
+    int big_number = 23;
+
+    // When executing
+    int result = max(small_number, big_number);
+
+    // Then the lower one is returned
+    TEST_ASSERT_EQUAL(big_number, result);
+}
+
+void app_main() {
+    UNITY_BEGIN();
+
+    RUN_TEST(test_function_min);
+    RUN_TEST(test_function_max);
+
+    UNITY_END();
+}
