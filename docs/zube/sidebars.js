d8435c7c77f08c913fad9223b8bcacbbf37b7d96
Michael Kargl <michael.kargl@posteo.net>
2022-09-10 12:55:46 +0300

    feat: adds draft of architecture vision to docusaurus docs
    - feat: adds reusable highlight JSX component
    - feat: adds reusable canvas banner JSX component for colorful headlines
    - feat: renames and reorders sidebars to provide a basic documentation structure
    - feat: moves mdx usage demo into styleguide
    - feat: adds presence-ui architecture vision
    - feat: adds presence bot system context
    
    part of #18

--- docs/zube/sidebars.js
+++ docs/zube/sidebars.js
@@ -1,6 +1,11 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-  },
+  presenceBotSidebar: {
+    PresenceBot: [ 
+      'presence-bot',
+      'presence-ui',
+      'presence-cube',
+      'presence-twin',
+      'markdown-styleguide',
+    ],
+  }
 };
