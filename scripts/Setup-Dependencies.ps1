d262b7b0b6ceb7b746cd7b96bc97a619eca67f05
Michael Kargl <michael.kargl@posteo.net>
2024-03-11 21:26:22 +0100

    feat: fixes setup-dependencies script to search idf-component configs - also adds comments to generated menu sections so that it's easier to upgrade out-of-date configurations

--- scripts/Setup-Dependencies.ps1
+++ scripts/Setup-Dependencies.ps1
@@ -1,3 +1,4 @@
+$ScriptName = $MyInvocation.MyCommand.Name
 $ProjectRoot = Split-Path -Parent $PSScriptRoot
 
 $SrcPath = Join-Path $ProjectRoot 'src'
@@ -33,9 +34,9 @@ Function Find-ConfigurationFiles {
     )
 
     PROCESS {
-        Get-ChildItem -File -Recurse:$Recurse `
-                    -Filter '*Kconfig.projbuild' `
-                    -LiteralPath $Path
+        Get-ChildItem -File -Recurse:$Recurse -LiteralPath $Path | Where-Object {
+            $_ -match 'Kconfig(.projbuild)?$'
+        }
     }
 }
 
@@ -53,13 +54,42 @@ Function Join-ConfigurationFile {
 
         # ESP-IDF can handle only UTF8 without BOM which powershell can't handle
         $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
-        [string[]]$NewLines = @([System.Environment]::NewLine*2)
+        [string[]]$NewLines = @([System.Environment]::NewLine * 2)
+        
+
+        # TODO: Before starting any concatenation, check if the the source menu entries
+        #       are already part of the target file. Otherwise you couldn't upgrade
+        #       library menus without removing all the menu entries. Create a ticket for
+        #       that and note that the upgrade instructions in the comment box (below)
+        #       will also have to be adjusted.
+
+        [System.IO.File]::AppendAllText($Path, @"
+        
+
+# ┌─────────────────────────────────────────────────────────────────────────────────────────┐
+# │                                                                                         │
+# │ │  Below you can find menu entries automatically concatenated from external sources     │
+# │ │  (ie. configuration items from external libraries or idf-components). To update /     │
+# │    them (after a library version upgrade for instance), remove ALL the concatenated     |
+# |    menu entries manually and re-run the config prep tool again ($ScriptName).
+# |    See README.                                                                          |
+# │                                                                                         │
+# └─────────────────────────────────────────────────────────────────────────────────────────┘
+
+"@);
     }
 
     PROCESS {
         Write-Verbose "Concatenating $FullName onto $Path"
 
         [string[]]$RawContent = @(Get-Content -Raw $FullName)
+        [System.IO.File]::AppendAllText($Path, @"
+# ┌─────────────────$('─'*$FullName.Length)─┐
+# │                 $(' '*$FullName.Length) │
+# │ Concated from $FullName   │
+# │                 $(' '*$FullName.Length) │
+# └─────────────────$('─'*$FullName.Length)─┘
+"@);
         [System.IO.File]::AppendAllLines($Path, $NewLines, $Utf8NoBomEncoding);
         [System.IO.File]::AppendAllLines($Path, $RawContent, $Utf8NoBomEncoding);
     }
@@ -73,4 +103,4 @@ Function Join-ConfigurationFile {
     $LibraryPath,
     $IdfComponentsPath
 ) | Find-ConfigurationFiles -Recurse `
-  | Join-ConfigurationFile -Verbose -Path $ConfigurationPath
+| Join-ConfigurationFile -Verbose -Path $ConfigurationPath
